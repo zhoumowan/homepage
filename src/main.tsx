03a68132040cbc3587d60b6327de9c44a440471b
zhoumowan <fanzong211@126.com>
2026-04-15 12:15:17 +0800

    fix: 解决部分浏览器 dvh 失效导致的视口高度问题

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+// 解决移动端 100vh 和 100dvh 依然可能存在的黑边问题
+function setVH() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setVH();
+window.addEventListener('resize', setVH);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
